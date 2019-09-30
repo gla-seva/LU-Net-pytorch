0b32e9d8ecf0457d529079481f2cc02f89439b9b
gla-seva <gla-seva@yandex.ru>
2019-09-30 17:24:57 +0300

    config added

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+#paths
+dataset = "/home/vglasov/datasets/L_CAS_3D_Point_Cloud_People_Dataset/3D Point Cloud People Dataset/"
+
+#Lidar image spec
+eps = 1e-6
+shape = (16, 384) #image shape
+angular = (0.5236, 2 * np.pi) # approximation of 30º and 360º
+
+
